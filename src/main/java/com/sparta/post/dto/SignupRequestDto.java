ca7de31a0a3cda73f78bc0043ff4d11ca9e78ce3
haeri <hadi43@naver.com>
2022-12-21 12:25:31 +0900

    정규식 표현 사용

--- src/main/java/com/sparta/post/dto/SignupRequestDto.java
+++ src/main/java/com/sparta/post/dto/SignupRequestDto.java
@@ -1,14 +1,28 @@
 package com.sparta.post.dto;
 
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
 @Setter
 @Getter
 public class SignupRequestDto {
+
+    @NotBlank
+    @Size(min=4,max=10)
+    @Pattern(regexp="^([a-z]+[0-9]*)$")
+    // 영소문자반드시 포함, 숫자가 있어도 됨
     private String username;
+
+    @NotBlank
+    @Size(min=8,max=15)
+    @Pattern(regexp="^(?=.*[A-Za-z])(?=.*\\d)(?=.*[$@$!%*#?&])[A-Za-z\\d$@$!%*#?&]$")
+    // 숫자, 영문자, 특수문자 각 1개 이상 포함
     private String password;
+
     private boolean admin = false;
     private String adminToken = "";
 }
